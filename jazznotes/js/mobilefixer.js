c2ecb8e021da44773215d840c787ec2c2e1d8b78
Seb Heron <sebheron21@hotmail.co.uk>
2021-03-17 00:15:06 +0000

    another attempt at fixing

--- jazznotes/js/mobilefixer.js
+++ jazznotes/js/mobilefixer.js
@@ -2,4 +2,5 @@ const nav_bar = document.querySelector(".top-bar");
 
 if (/Mobi|Android/i.test(navigator.userAgent)) {
   nav_bar.style.position = "static";
+  document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
 }
