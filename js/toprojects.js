8455093f1b5f828736efc72b7a20e9f7b162a46f
Seb Heron <sebheron21@hotmail.co.uk>
2021-07-30 22:08:22 +0100

    Improved navigation

--- js/toprojects.js
+++ js/toprojects.js
@@ -7,13 +7,36 @@ const page_transition = document.getElementById('page-transition');
 function navigateToProjects() {
     page_transition.style.animation = 'growIn 500ms linear forwards';
     window.setTimeout(function () {
+        window.history.pushState("", "", '#projects');
         home_page.style.display = 'none';
         projects_page.style.display = 'block';
         page_transition.style.animation = 'growOut 500ms linear forwards';
     }, 600);
 }
 
+function navigateToHome() {
+    page_transition.style.animation = 'growIn 500ms linear forwards';
+    window.setTimeout(function () {
+        home_page.style.display = '';
+        projects_page.style.display = '';
+        page_transition.style.animation = 'growOut 500ms linear forwards';
+    }, 600);
+}
+
+window.onhashchange = function () {
+    if (window.location.hash.includes('projects')) {
+        navigateToProjects();
+    }
+    else {
+        navigateToHome();
+    }
+}
+
 function start() {
+    if (window.location.hash.includes('projects')) {
+        home_page.style.display = 'none';
+        projects_page.style.display = 'block';
+    }
     project_button.addEventListener("click", navigateToProjects);
 }
 
