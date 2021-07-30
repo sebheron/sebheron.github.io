a4d4435549ab329dde521604bf70ead106d111d3
Seb Heron <sebheron21@hotmail.co.uk>
2021-07-30 02:55:40 +0100

    Began projects page and added transition

---
+++ js/toprojects.js
@@ -0,0 +1,24 @@
+const project_button = document.getElementById('projects-button');
+const page_transition = document.getElementById('page-transition');
+
+function showFade() {
+    if (location.href.includes('portfolio') && location.hash == '#kms') {
+        window.setTimeout(function () {
+            page_transition.style.animation = 'growOut 500ms linear forwards';
+        }, 300);
+    }
+}
+
+function navigateToProjects() {
+    page_transition.style.animation = 'growIn 500ms linear forwards';
+    window.setTimeout(function () {
+        window.location.href = 'file:///C:/Users/Sebhe/Documents/GitHub/swegrock.github.io/portfolio.html#kms';
+    }, 500);
+}
+
+function start() {
+    showFade();
+    project_button.addEventListener("click", navigateToProjects);
+}
+
+document.addEventListener("DOMContentLoaded", start, false);
